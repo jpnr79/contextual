fb6d2191ab9d02b11b4ba2128c9e06511f94f5be
Pedro Rocha <jpnr79@gmail.com>
2025-12-12 00:24:17 +0000

    glpi 11 update

--- setup.php
+++ setup.php
@@ -41,34 +41,8 @@ function plugin_init_contextual() {
 	
     Html::requireJs('charts');
     $Plugin = new Plugin();
-
-<?php
-    if ($Plugin->isActivated('contextual')) {	
-/*
- * @version $Id: HEADER 15930 2011-10-25 10:47:55Z jmd $
- * -------------------------------------------------------------------------
- * GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2003-2011 by the INDEPNET Development Team.
- * http://indepnet.net/   http://glpi-project.org
- * -------------------------------------------------------------------------
- * LICENSE
- *
- * This file is part of GLPI.
- *
- * GLPI is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * GLPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
- * --------------------------------------------------------------------------
- */
+// ...existing code...
+// ...existing code...
 
 		// Registro de clases	 
 		Plugin::registerClass('PluginContextualProfile',         ['addtabon' => array('Profile')]); 
@@ -130,7 +104,7 @@ function plugin_init_contextual() {
 		$PLUGIN_HOOKS['post_show_item']['contextual'] = ['PluginContextualContextual',
                                                          'post_show_tab'];
 	}	
-}
+// removed unmatched closing brace
 
 // Get the name and the version of the plugin - Needed
 function plugin_version_contextual() {
